a49e413d126984a7dc2ab49e1ce75877456043b3
Artemii Korshunov <pon4ickoff@yandex.ru>
2023-03-27 17:57:35 +0300

    added docker file and react js example

--- backend/app.js
+++ backend/app.js
@@ -15,13 +15,12 @@ const iceberg1 = require("./routes/iceberg/iceberg_1");
 const iceberg2 = require("./routes/iceberg/iceberg_2");
 const iceberg3 = require("./routes/iceberg/iceberg_3");
 
-
 const carbondioxide = require("./routes/carbondioxide/carbondioxide");
 const visualization4 = require("./routes/visualization4/visualization4.js");
 
-const sector = require("./routes/visualization5/sector.js")
-const sub_sector = require("./routes/visualization5/sub_sector.js")
-const sub_sector_further = require("./routes/visualization5/sub_sector_further.js")
+const sector = require("./routes/visualization5/sector.js");
+const sub_sector = require("./routes/visualization5/sub_sector.js");
+const sub_sector_further = require("./routes/visualization5/sub_sector_further.js");
 
 var app = express();
 
@@ -48,9 +47,8 @@ app.use("/iceberg1", iceberg1);
 app.use("/iceberg2", iceberg2);
 app.use("/iceberg3", iceberg3);
 
-
-app.use("/carbondioxide",carbondioxide);
-app.use("/visualization4", visualization4)
+app.use("/carbondioxide", carbondioxide);
+app.use("/visualization4", visualization4);
 
 app.use("/sector", sector);
 app.use("/sub_sector", sub_sector);
@@ -59,6 +57,11 @@ app.use("/sub_sector_further", sub_sector_further);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
+});
+
+const port = 229;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
 
 module.exports = app;
