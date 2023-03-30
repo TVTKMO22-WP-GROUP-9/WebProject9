e8dbd4079d32f2c5c58cd9a164d738a14ccde226
Artemii Korshunov <pon4ickoff@yandex.ru>
2023-03-30 17:18:43 +0300

    added backend for user, and front end

---
+++ forntend/src/Data.js
@@ -0,0 +1,33 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2016,
+        userGain: '80000',
+        userLost: '823',
+    },
+    {
+        id: 2,
+        year: 2017,
+        userGain: '90000',
+        userLost: '345',
+    },
+    {
+        id: 3,
+        year: 2018,
+        userGain: '100000',
+        userLost: '123',
+    },
+    {
+        id: 4,
+        year: 2019,
+        userGain: '110000',
+        userLost: '456',
+    },
+    {
+        id: 5,
+        year: 2020,
+        userGain: '30000',
+        userLost: '789',
+    },
+];
+
